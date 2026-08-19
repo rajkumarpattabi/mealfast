18dc5b29e0362cfe639166790a9fe339dfa71882
rajkumarpattabi <rajkumar.com@gmail.com>
2026-08-19 12:27:12 +0530

    remove hint line

--- sw.js
+++ sw.js
@@ -10,7 +10,7 @@
  * Bump CACHE_NAME on every deploy: the new worker installs the new cache and
  * the activate handler deletes the old one. (This is the "vNN" you increment.)
  * ==========================================================================*/
-const CACHE_NAME = "mealfast-v61";
+const CACHE_NAME = "mealfast-v62";
 const ASSETS = [
   "./",
   "./index.html",
